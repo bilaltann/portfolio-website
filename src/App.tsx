12e52006b2f0973a8b7359ec6b0f7ec867f324c9
Bilal Tan <bilaltan070@gmail.com>
2026-09-01 00:50:50 +0300

    fetch URL redirected to Render address

--- src/App.tsx
+++ src/App.tsx
@@ -435,7 +435,7 @@ const Contact = () => {
 
     try {
 
-      const response = await fetch(`/api/contact`, {
+      const response = await fetch(`https://portfolio-mail-api-97ro.onrender.com/api/contact`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
